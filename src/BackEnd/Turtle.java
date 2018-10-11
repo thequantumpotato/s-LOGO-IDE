1a6ab7f204878159f10b7cef19001ea4a92e149c
Harry Xie <harryjordan9208@gmail.com>
2018-10-11 17:26:01 -0400

    turtle first commit

--- src/BackEnd/Turtle.java
+++ src/BackEnd/Turtle.java
@@ -1,4 +1,31 @@
 package BackEnd;
 
+/**
+ * represents a turtle on the screen
+ */
 public class Turtle {
+
+    private double direction;
+    private double size;
+    private Color color;
+    private ImageView imageView;
+    private Pen pen;
+
+    public Turtle(){
+
+    }
+
+    public void move(double distance){
+        imageView.setX(imageView.getX() + distance);
+    }
+
+    public void turn(double angle){
+        direction += angle;
+    }
+
+    public void drawCircle(double centerX, double centerY, double radius){
+
+    }
+
+
 }
