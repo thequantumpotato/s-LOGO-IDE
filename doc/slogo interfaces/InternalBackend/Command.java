df3c6506afda18f523516fef53f699f4ae223f7b
josesanmartin <js665@duke.edu>
2018-10-12 17:17:51 -0400

    Slogo interfaces

---
+++ doc/slogo interfaces/InternalBackend/Command.java
@@ -0,0 +1,10 @@
+//Internal Backend API 
+
+//This class is what actually runs our turtle. It holds a reference to the turtle, and through the process
+//of reflection, it's methods are called within Controller
+
+interface Command {
+	public void Forward();
+
+	public void Backward();
+}
