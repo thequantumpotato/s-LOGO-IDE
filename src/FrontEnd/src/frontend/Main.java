21a64f0c13a8428197d2e870503700f839cdc824
thequantumpotato <thequantumpotato@gmail.com>
2018-10-16 21:13:33 -0400

    changes to turtle display, added turtle movement and animation

--- src/FrontEnd/src/frontend/Main.java
+++ src/FrontEnd/src/frontend/Main.java
@@ -7,6 +7,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setResizable(false);
         Controller myController = new Controller(primaryStage);
     }
 
