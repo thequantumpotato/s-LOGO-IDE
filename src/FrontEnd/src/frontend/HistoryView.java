a981405702155592c314a389ae0c211e059f6b2e
vincentliu98 <jie.liu0731@gmail.com>
2018-10-17 20:39:33 -0400

    added historyView

--- src/FrontEnd/src/frontend/HistoryView.java
+++ src/FrontEnd/src/frontend/HistoryView.java
@@ -1,18 +1,33 @@
 package frontend;
 
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class HistoryView implements SubView {
-    private ScrollPane historyView;
+    private VBox historyView;
+    private ScrollPane historyPane;
+    private VBox history;
     private View myView;
+    private List<String> myHistory;
 
     public HistoryView(View myView_){
         myView = myView_;
-        historyView = new ScrollPane();
+        historyPane = new ScrollPane();
+        historyPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+
+        historyView = new VBox();
         historyView.getStyleClass().add("historyView");
+
+        Label title = new Label("Command History");
+        historyView.getChildren().addAll(title, historyPane);
     }
 
+
+    
     @Override
     public Node getView() {
         return historyView;
