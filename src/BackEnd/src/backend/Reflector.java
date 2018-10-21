dcebf9a199a7af180b9538fed97ff7608f934482
josesanmartin <js665@duke.edu>
2018-10-21 17:10:00 -0400

    changes to reflector and command constructors

--- src/BackEnd/src/backend/Reflector.java
+++ src/BackEnd/src/backend/Reflector.java
@@ -8,7 +8,9 @@ import backend.Nodes.ArgumentNode;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Jose San Martin
@@ -20,46 +22,48 @@ public class Reflector {
 
     private Command Commander;
     private Turtle myTurtle;
+    private Map<String, ArgumentNode> variableMap = new HashMap<>();
+    private Map<String, CommandNode> instructionMap = new HashMap<>();
 
-    public Reflector(Command command, Turtle turtle){
-        Commander = command;
+    public Reflector(Turtle turtle) {
         myTurtle = turtle;
+        Commander = new Command(this, myTurtle);
     }
 
     public void execute(BasicNode root) throws NoSuchMethodException {
-        if(root.getCommandName().matches("If")){
+        if (root.getCommandName().matches("If")) {
             handleIf(root);
-        }
-        else{
+        } else {
             traverseTree(root);
         }
     }
 
-    private BasicNode traverseTree(BasicNode root) throws NoSuchMethodException{
-        if (root == null){
+    private BasicNode traverseTree(BasicNode root) throws NoSuchMethodException {
+        if (root == null) {
             return null;
         }
         System.out.println(root instanceof CommandNode);
-        if(isNotCommand(root)){ //Continue only if the node is part of the Command hierarchy
+        if (isNotCommand(root)) { //Continue only if the node is part of the Command hierarchy
             return root;
         }
         List<BasicNode> children = new ArrayList<>();
-        for(BasicNode child: root.getChildren()){
+        for (BasicNode child : root.getChildren()) {
             children.add(traverseTree(child));
         }
-        Method command = Commander.getClass().getDeclaredMethod(root.getCommandName(), Turtle.class, List.class);
+        String name = root.getCommandName().substring(0, 1).toLowerCase() + root.getCommandName().substring(1);
+        Method command = Commander.getClass().getDeclaredMethod(name, List.class);
+
         //Invoke the command, and obtain the returned value, which is turned into a new argument node
-        try{
+        try {
             myTurtle.Changed();
-            BasicNode ret = (BasicNode) command.invoke(children);
+            BasicNode ret = (BasicNode) command.invoke(Commander, children);
             myTurtle.clear();
-            if(ret instanceof LoopNode){ //If it isnt an arg node, it is a loop node!
-                loop((LoopNode)ret);
+            if (ret instanceof LoopNode) { //If it isnt an arg node, it is a loop node!
+                loop((LoopNode) ret);
             }
             return ret;
 
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         //Return the new argument node
@@ -67,16 +71,16 @@ public class Reflector {
     }
 
     private BasicNode handleIf(BasicNode root) throws NoSuchMethodException {
-        if (root == null){
+        if (root == null) {
             return null;
         }
-        if(isNumeric(root.getCommandName())){
+        if (isNumeric(root.getCommandName())) {
             return root;
         }
         //The first child is always the condition to be evaluated
         BasicNode condition = traverseTree(root.getChildren().get(0));
         BasicNode lastCommand = null;
-        if(!condition.getCommandName().equals("0")){
+        if (!condition.getCommandName().equals("0")) {
             BasicNode list = root.getChildren().get(1);
             lastCommand = loopList(list);
         }
@@ -86,27 +90,71 @@ public class Reflector {
     //TODO: Make this return the last value of the last executed command
     public void loop(LoopNode loopNode) throws NoSuchMethodException { //Have to accept LoopNode, not BasicNode
         //Run the commands in the list a specified number of times
-        for(int i=0; i<loopNode.getReps();i++){
-            for(BasicNode subCommand:loopNode.getChildren()){
+        for (int i = 0; i < loopNode.getReps(); i++) {
+            for (BasicNode subCommand : loopNode.getChildren()) {
                 execute(subCommand);
             }
         }
         return;
     }
+
     public BasicNode loopList(BasicNode list) throws NoSuchMethodException {
         BasicNode lastCommand = null;
-        for(BasicNode commandToRun : list.getChildren()){
+        for (BasicNode commandToRun : list.getChildren()) {
             lastCommand = traverseTree(commandToRun);
         }
         return lastCommand;
     }
 
-    private boolean isNumeric(String s){
-        return s.matches("[-+]?\\d*\\.?\\d+");
+    public boolean createVariable(String name) {
+        if (variableMap.keySet().contains(name)) {
+            return false;
+        }
+        variableMap.put(name, new ArgumentNode("0.0"));
+        return true;
     }
 
-    private boolean isNotCommand(BasicNode n){
-        return !(n instanceof CommandNode);
+    public boolean setVariable(String name, ArgumentNode value) {
+        if (!variableMap.keySet().contains(name)) {
+            return false;
+        }
+        variableMap.put(name, value);
+        return true;
     }
+
+    public ArgumentNode getVariable(String name) {
+        if (!variableMap.keySet().contains(name)) {
+            return null;
+        }
+        return variableMap.get(name);
+    }
+
+    public boolean createInstruction(String name, CommandNode inst) {
+        if (instructionMap.keySet().contains(name)) {
+            return false;
+        }
+        instructionMap.put(name, inst);
+        return true;
+    }
+
+    public BasicNode getInstruction(String name) {
+        if (!instructionMap.keySet().contains(name)) {
+            return null;
+        }
+        return instructionMap.get(name);
+
+    }
+        private boolean isNumeric (String s){
+            return s.matches("[-+]?\\d*\\.?\\d+");
+        }
+
+        private boolean isNotCommand (BasicNode n){
+            return !(n instanceof CommandNode);
+        }
+    public void clearScreen(){
+
+    }
+
+
 
 }
