801bf5606ec0ba167091518805fe27073a7d4a84
vincentliu98 <jie.liu0731@gmail.com>
2018-10-23 10:36:58 -0400

    deleted interfaces created 2 weeks ago

--- doc/slogo interfaces/ExternalFrontend/IllegalCommandException.java
+++
@@ -1,6 +0,0 @@
-//This class handles throws an exception when the command input is not valid
-
-interface IllegalCommandException implements Exception {
-
-    public String throwException(String errormsg);
-}
