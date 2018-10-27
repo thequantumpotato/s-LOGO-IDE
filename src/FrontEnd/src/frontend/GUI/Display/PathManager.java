ab3fcb5a3ba3e95cada3a277fb1b91d03a2fb54f
Ben Xu <benjamin.xu@duke.edu>
2018-10-27 15:17:21 -0400

    refactoring statics to more logical classes

--- src/FrontEnd/src/frontend/GUI/Display/PathManager.java
+++ src/FrontEnd/src/frontend/GUI/Display/PathManager.java
@@ -11,12 +11,13 @@ import java.util.ArrayList;
 /** Class for the management of line drawing on the display
  *  @author bpx */
 public class PathManager extends Manager{
+
     public static final String PATH_KEY = "path";
+
     private ArrayList<Path> myPaths;
     private AnimationContainer myAnimationContainer;
 
-
-
+    /** Default constructor */
     public PathManager(){
         super();
         myPaths =  new ArrayList<>();
@@ -24,6 +25,8 @@ public class PathManager extends Manager{
 
     }
 
+    /** Adds any number of {@code Line} objects to the {@code Path} and generates the appropriate {@PathTracer} objects
+     *  @param lines The {@code Line} objects that will be added to the {@code Path}*/
     public void addPath(Line...lines){
         LinePath newpath = new LinePath();
         for(Line l : lines){
@@ -34,6 +37,7 @@ public class PathManager extends Manager{
         myAnimationContainer.addAnimation(PATH_KEY,pathTracer.getTracer());
     }
 
+    /** Resets {@code PathManager} fields to default state */
     @Override
     public void reset(){
         myPaths.clear();
--- src/FrontEnd/src/frontend/GUI/Display/TurtleManager.java
+++ src/FrontEnd/src/frontend/GUI/Display/TurtleManager.java
@@ -2,26 +2,45 @@ package frontend.GUI.Display;
 
 
 import frontend.Util.AnimationContainer;
+import javafx.scene.Group;
 
+/** Manages turtles on the display
+ *  @author bpx */
 public class TurtleManager extends Manager {
 
+    public static final int DEFAULT_DURATION = 2;
+
+    private SpriteManager mySpriteManager;
     private AnimationContainer myAnimationContainer;
     private MovementManager myMovementManager;
     private PathManager myPathManager;
 
-    public TurtleManager(){
+    public TurtleManager(Group renderTarget){
         super();
+        mySpriteManager = new SpriteManager(renderTarget);
+        myAnimationContainer = new AnimationContainer();
+        myMovementManager = new MovementManager();
+        myPathManager =  new PathManager();
+    }
+
+    public void createTurtle(){
+
     }
 
 
 
     @Override
     public void setDuration(double duration){
-
+        super.setDuration(duration);
+        myMovementManager.setDuration(duration);
+        myPathManager.setDuration(duration);
     }
 
     @Override
     public void reset() {
-
+        mySpriteManager.killAllTurtles();
+        myAnimationContainer.killAllAnimations();
+        myMovementManager.reset();
+        myPathManager.reset();
     }
 }
