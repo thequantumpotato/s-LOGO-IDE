1c6b6a14c21a59a75680956cc0e1475679e237e0
vincentliu98 <jie.liu0731@gmail.com>
2018-10-28 22:52:30 -0400

    refactored stateView

--- src/FrontEnd/src/frontend/GUI/SubViews/StateView.java
+++ src/FrontEnd/src/frontend/GUI/SubViews/StateView.java
@@ -2,7 +2,6 @@ package frontend.GUI.SubViews;
 
 import frontend.API.SubView;
 import frontend.GUI.Display.PathManager;
-import frontend.GUI.View;
 import frontend.Util.Pen;
 import frontend.Util.Sprite;
 import javafx.scene.Node;
@@ -12,11 +11,17 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
-
-// onclick/hover on a turtle's x and y (javafx coor vs. relative coor)
-// -> get turtle from backend -> pass all the information of the turtle through receiveTurtle(Turtle)
-// -> present the information
-
+/**
+ * The panel that shows all the information about the turtle and the pen.
+ *
+ * When the mouse is not hovering above a turtle, the StateView will display just the attribute name
+ * but not the content.
+ *
+ * When the mouse is hovering above a turtle, StateView show turtle's x and y and direction,
+ * its active state, and the pen's size, color, and whether the pen is up or down.
+ *
+ * @author Vincent Liu
+ */
 public class StateView implements SubView {
     private VBox stateView = new VBox();
     private ListView stateList = new ListView();
@@ -27,6 +32,10 @@ public class StateView implements SubView {
         showDefault();
     }
 
+    /**
+     * Only show the attribute name but not the content.
+     * This method is called when the mouse exits the Sprite.
+     */
     public void showDefault(){
         stateList.getItems().clear();
         setUpTurtleFields();
@@ -48,25 +57,43 @@ public class StateView implements SubView {
         stateList.getItems().addAll(idT, xT, yT, directionT);
     }
 
+    /**
+     * This command is passed by View from the TurtleManager when the mouse is hovering above a Sprite.
+     * It changes the information presented on the StateView
+     * @param id
+     * @param sprite
+     * @param pathManager
+     */
     public void changeState(String id, Sprite sprite, PathManager pathManager){
         stateList.getItems().clear();
         updateTurtleVars(id, sprite);
         updatePenVars(pathManager);
     }
 
+    /**
+     * After receiving information about the Sprite and the turtle id, extract necessary information to present,
+     * including the x, y, direction, and of course, id itself
+     * @param id
+     * @param sprite
+     */
+    private void updateTurtleVars(String id, Sprite sprite) {
+        double x = sprite.getMyX();
+        double y = sprite.getMyY();
+        double direction = sprite.getMyAngle();
+        setUpdatedTurtleFields(id, x, y, direction);
+    }
+
+    /**
+     * After receiving information about the PathManager, extract necessary information to present, including
+     * pen's state (down or up), color, and size
+     * @param pathManager
+     */
     private void updatePenVars(PathManager pathManager) {
         Pen myPen = pathManager.getMyPen();
         boolean penDown = pathManager.isPenDown();
         Color penColor = myPen.getMyColor();
         double penSize = myPen.getMySize();
         setUpdatedPenFields(penDown, penColor, penSize);
-    }
-
-    private void updateTurtleVars(String id, Sprite sprite) {
-        double x = sprite.getMyX();
-        double y = sprite.getMyY();
-        double direction = sprite.getMyAngle();
-        setUpdatedTurtleFields(id, x, y, direction);
     }
 
     public void setUpdatedPenFields(boolean penDown, Color penColor, double penSize){
