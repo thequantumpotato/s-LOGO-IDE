79932457cb422290dfa9cbe2e997beab1ab3174d
josesanmartin <js665@duke.edu>
2018-10-29 02:17:12 -0400

    nested for/repeat loops working

--- src/BackEnd/src/backend/Commands/Forward.java
+++ src/BackEnd/src/backend/Commands/Forward.java
@@ -14,7 +14,6 @@ public class Forward extends RootNode{
     @Override
     public Object run() {
         List<Double> l = parseDoubles(runChildren());
-        System.out.println(l);
         myTurtle.Changed();
         myTurtle.move(l.get(0));
         myTurtle.clear();
