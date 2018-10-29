7a8f532937523f471d772b3ee4c581ebca3f3be5
josesanmartin <js665@duke.edu>
2018-10-29 14:26:30 -0400

    Bug fixes

--- src/BackEnd/src/backend/TreeFactory.java
+++ src/BackEnd/src/backend/TreeFactory.java
@@ -78,20 +78,21 @@ public class TreeFactory {
             return null;
         }
         Node newChild;
-        String nextChild = commands.remove(0);
         //if (isLeftParenthesis(nextChild)) { //check if its a parenthesis FIRST
         //    String nextCommand = commands.remove(0);
         //    newChild = createRoot(nextCommand);
         //    generateCommand(newChild, commands, getArgNum(nextCommand));}
-        if (isOpenBracket(nextChild)) {
+        if (isOpenBracket(commands.get(0))) {
             newChild = generateList(commands);
             commands.remove(0); //Remove that ending bracket!
         }
-        else if (!isNotCommand(nextChild) & getArgNum(nextChild)!= 99) {
+        else if (!isNotCommand(commands.get(0)) & getArgNum(commands.get(0))!= 99) {
+            String nextChild = commands.remove(0);
             newChild = createRoot(nextChild);
             //This child has its own arguments that we need to add. Use recursion!
             generateCommand(newChild, commands, getArgNum(nextChild));
         } else {
+            String nextChild = commands.remove(0);
             newChild = getLeafNode(nextChild);
         }
 
